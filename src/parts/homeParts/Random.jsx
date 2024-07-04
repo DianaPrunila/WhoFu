22251697dd6f5be7dbce5f7e7ed8fa2eeefdfc4c
DianaPrunila <pruniladiana03@yahoo.it>
2024-07-04 16:59:07 +0200

    Random fixed

--- src/parts/homeParts/Random.jsx
+++ src/parts/homeParts/Random.jsx
@@ -18,42 +18,42 @@ const Random = () => {
   const adding = (data) => {
     dispatch(add(data));
     dispatch(calculateTotalPrice());
-    return (
-      <>
-        <h2 className="flex flex-col flex-wrap  mb-4 content-center text-bold">
-          DEAL OF THE DAY
-        </h2>
-        <div className=" mb-4 d-flex flex-col flex-wrap  content-center">
-          <div className="w-52 shadow-lg rounded-xl ">
-            <div className="d-flex justify-center items-center ">
-              <img
-                src={imageP}
-                alt={nameP}
-                className="h-44 object-cover d-flex items-center rounded-xl mt-2"
-              />
-            </div>
+  };
+  return (
+    <>
+      <h2 className="flex flex-col flex-wrap  mb-4 content-center text-bold">
+        DEAL OF THE DAY
+      </h2>
+      <div className=" mb-4 d-flex flex-col flex-wrap  content-center">
+        <div className="w-52 shadow-lg rounded-xl ">
+          <div className="d-flex justify-center items-center ">
+            <img
+              src={imageP}
+              alt={nameP}
+              className="h-44 object-cover d-flex items-center rounded-xl mt-2"
+            />
+          </div>
 
-            <div className="d-flex flex-col h-40 items-center pt-4 mb-2">
-              <p className="p-0 m-0">{nameP}</p>
-              <p className="text-lightGreen text-xl font-semibold p-0 m-0">
-                {priceP}
-              </p>
-              <p className="p-0 m-0">{quantityP}</p>
+          <div className="d-flex flex-col h-40 items-center pt-4 mb-2">
+            <p className="p-0 m-0">{nameP}</p>
+            <p className="text-lightGreen text-xl font-semibold p-0 m-0">
+              {priceP}
+            </p>
+            <p className="p-0 m-0">{quantityP}</p>
 
-              <button
-                className="bottom-center p-2 rounded-xl mt-2 bg-lightGreen
+            <button
+              className="bottom-center p-2 rounded-xl mt-2 bg-lightGreen
                     hover:bg-navBg text-white  active:bg-lightGreen text-bold "
-                onClick={() => adding(PRODUCTS[randomIndex])}
-                style={randomWriting}
-              >
-                Add to cart
-              </button>
-            </div>
+              onClick={() => adding(PRODUCTS[randomIndex])}
+              style={randomWriting}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
 };
 
 export default Random;
